6a6a0193ae3b7b911cf46ee7f7803d97267e49bf
Fatima Zahra <ait29lamine5@gmail.com>
2025-10-14 20:24:17 +0100

    created presentation file using spring

---
+++ src/main/java/pres/Pres2_spring.java
@@ -0,0 +1,13 @@
+package pres;
+
+import metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Pres2_spring {
+    public static void main(String[] args) {
+        ApplicationContext springContext = new ClassPathXmlApplicationContext("config.xml");
+        IMetier metier = (IMetier) springContext.getBean("metier");
+        System.out.println("Resultat : " + metier.calcul());
+    }
+}
